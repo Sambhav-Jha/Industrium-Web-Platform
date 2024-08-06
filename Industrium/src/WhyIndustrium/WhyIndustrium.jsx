dd6021f5eaa0c4b0ce595fb74a38f46f8a60d2cc
Goutham-IITJ <b23ee1024@iitj.ac.in>
2024-08-07 00:50:45 +0530

    Added the  why section, including layout and hover effects

--- Industrium/src/WhyIndustrium/WhyIndustrium.jsx
+++ Industrium/src/WhyIndustrium/WhyIndustrium.jsx
@@ -1,7 +1,36 @@
-export default function WhyIndustrium(){
-    return(
-        <>
-            <p>WhyIndustrium page</p>
-        </>
-    )
+import './WhyIndustrium.css';
+
+export default function WhyIndustrium() {
+  return (
+    <div className="why">
+      <h2>WHY INDUSTRIUM</h2>
+      <div className="content">
+        <div className="box">
+          <h3>Welcome to Industrium</h3>
+          <p>
+            "  In the fast-growing industrial sector,
+            access to accurate and up-to-date information on industrial parts
+            parts is crucial. Industrium revolutionizes sourcing by automating
+            data collection, ensuring reliable, comprehensive, and accessible
+            data for businesses and individuals. Utilizing a combination of web
+            scraping techniques and machine learning algorithms, the platform
+            will search through news sites, recent media coverage and other
+            appropriate web sources to provide relevant supplier information. "
+          </p>
+        </div>
+        <div className="box">
+          <h3>The Indian industrial sector is booming:</h3>
+          <ul>
+            <li>Manufacturing Contribution to GDP: 16-17%</li>
+            <li>Market Size for Industrial Machinery by 2024: USD 44 billion</li>
+            <li>Manufacturing Growth Rate (CAGR 2023-2028): 7.1%</li>
+            <li>Manufacturers Facing Sourcing Difficulties: Over 60%</li>
+          </ul>
+        </div>
+        <div className="box">
+          <p>....</p>
+        </div>
+      </div>
+    </div>
+  );
 }
